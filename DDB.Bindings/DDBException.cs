ccc6a6d34260082bb84146726264b827f10da6e6
Luca Di Leo <ldileo@digipa.it>
2020-10-23 16:13:42 +0200

    Migration from DroneDB

---
+++ DDB.Bindings/DDBException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDB.Bindings
+{
+    public class DDBException : Exception
+    {
+        public DDBException()
+        {
+
+        }
+        
+        public DDBException(string message) : base(message)
+        {
+
+        }
+
+        public DDBException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+    }
+}
