cc8976cff41b99d17767cc96714dbb1d217964b7
Luca Di Leo <ldileo@digipa.it>
2021-04-01 19:53:38 +0200

    Added utils

---
+++ DDB.Bindings/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.Bindings
+{
+    public static class Utils
+    {
+        private static readonly DateTime UnixEpoch = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static DateTime UnixTimestampToDateTime(long timestamp)
+        {
+            return UnixEpoch.Add(TimeSpan.FromSeconds(timestamp)).ToLocalTime();
+        }
+
+    }
+}
