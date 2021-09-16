b43e9dbe7baf1f4b39adf92e452fbe89653a8ba9
Luca Di Leo <admin@lucadileo.it>
2021-09-16 16:19:21 +0200

    Changed Data type to string

--- DDB.Bindings/Model/Meta.cs
+++ DDB.Bindings/Model/Meta.cs
@@ -7,7 +7,7 @@ namespace DDB.Bindings.Model
     public class Meta
     {
         public string Id { get; set; }
-        public JToken Data { get; set; }
+        public string Data { get; set; }
 
         [JsonProperty("mtime")]
         [JsonConverter(typeof(SecondEpochConverter))]
