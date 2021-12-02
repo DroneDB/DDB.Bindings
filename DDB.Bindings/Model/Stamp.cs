8736b42c8d5f7755cd9765f53bba9dfb9d34a455
Piero Toffanin <pt@masseranolabs.com>
2021-12-02 15:23:40 -0500

    Add stamp model

---
+++ DDB.Bindings/Model/Stamp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace DDB.Bindings.Model
+{
+
+    public class Stamp
+    {
+        public string Checksum { get; set; }
+
+        public List<Dictionary<string,string>> Entries { get; set; }
+
+        
+    }
+
+}
